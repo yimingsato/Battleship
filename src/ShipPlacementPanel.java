b2e075fd3c2f5fad53d228347be8bb7b9f4013b6
Lucas Chen <yimingsato@gmail.com>
2025-06-10 17:41:40 -0400

    continued GUI

--- src/ShipPlacementPanel.java
+++ src/ShipPlacementPanel.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class ShipPlacementPanel extends JPanel {
     
@@ -9,6 +10,25 @@ public class ShipPlacementPanel extends JPanel {
         JButton doneButton = new JButton("Done");
         doneButton.setActionCommand("done");
         doneButton.addActionListener(gui);
+        this.setLayout(new FlowLayout());
+        JPanel board = new JPanel(new GridLayout(10, 10));
+
+        Color lightSquare = Color.WHITE;
+        Color darkSquare = Color.BLACK;
+
+        for (int row = 0; row < 10; row++) {
+            for (int col = 0; col < 10; col++) {
+                JLabel square = new JLabel();
+                square.setPreferredSize(new Dimension(40, 40));
+                if ((row + col) % 2 == 0) {
+                    square.setBackground(new Color(100, 20, 70));
+                } else {
+                    square.setBackground(new Color(100, 20, 10));
+                }
+                board.add(square);
+            }
+        }
+        this.add(board);
         this.add(doneButton);
     }
 }
